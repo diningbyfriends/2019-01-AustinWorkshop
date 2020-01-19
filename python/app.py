23a830a904c03750a51dac784186fa08327ab762
Josh Perryman <joshperryman@gmail.com>
2020-01-19 15:49:31 -0600

    python style updates

--- python/app.py
+++ python/app.py
@@ -2,33 +2,32 @@ from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from gremlin_python.process.graph_traversal import __
 
-
 import json
 
 to_string = json.dumps
 
 class App(object):    
-    def connectToDatabase(self):
-        return DriverRemoteConnection('ws://db:8182/gremlin', 'g')
+    def connect_to_database(self):
+        return DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
 
-    def getGraphTraversalSource(self, cluster):
+    def get_graph_traversal_source(self, cluster):
         return traversal().withRemote(cluster)
 
-    def displayMenu(self, g):
-        option = int(self.showMenu())
-        if option==0:
+    def display_menu(self, g):
+        option = int(self.show_menu())
+        if option == 0:
             exit()
-        elif option==1:
-            print(self.getPerson(g))
-        elif option==2:
-            print(self.getFriends(g))
-        elif option==3:
-            print(self.getFriendsOfFriends(g))
+        elif option == 1:
+            print(self.get_person(g))
+        elif option == 2:
+            print(self.get_friends(g))
+        elif option == 3:
+            print(self.get_friends_of_friends(g))
         else:
             print("Sorry, please enter valid Option")
-        self.displayMenu(g)
+        self.display_menu(g)
 
-    def showMenu(self):
+    def show_menu(self):
         print("Main Menu:")
         print("--------------")
         print("1) Find person by name")
@@ -39,29 +38,29 @@ class App(object):
         print("Enter your choice:")
         return raw_input();
 
-    def getPerson(self, g):
+    def get_person(self, g):
         name = raw_input("Enter the first name of for the person to find:");
         vertex = g.V().has("person", "first_name", name).next();
         return vertex
 
-    def getFriends(self, g):
-        name=raw_input("Enter the name of the person to get their friends: ");
+    def get_friends(self, g):
+        name = raw_input("Enter the name of the person to get their friends: ");
         friends = g.V().has("person", "first_name", name).\
             both("friends").\
             dedup().\
             values("first_name").toList();
         return '\n'.join(friends);
 
-    def getFriendsOfFriends(self, g):
+    def get_friends_of_friends(self, g):
         name = raw_input("Enter the name of the person to get their friends: ");
         friends = g.V().has("person", "first_name", name).repeat(__.both("friends") ).times(2).dedup().values("first_name").toList();
         return '\n'.join(friends);
 
 def main():
     app = App()
-    cluster = app.connectToDatabase()
-    g = app.getGraphTraversalSource(cluster)
-    app.displayMenu(g)
+    cluster = app.connect_to_database()
+    g = app.get_graph_traversal_source(cluster)
+    app.display_menu(g)
 
 if __name__ == "__main__":
     main()
